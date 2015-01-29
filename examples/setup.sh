92450ae6aa86cf16bba686211a515309aaa9fb01
Makis Tracend <makis.tracend@gmail.com>
2015-01-29 01:21:28 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the L10N.website CLI
+
+# 1. Install the main executable
+npm install -g l10n-cli
+
+#2. Setup L10N.website (only needed once)
+l10n setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+l10n -h
